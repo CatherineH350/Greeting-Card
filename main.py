7ec32659bbc9bf2bd22b8229cfde9602035ef725
Catherine H <110302@eesd.org>
2021-03-25 23:09:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import turtle
+
+#turtle.Screen() allows you to define your screen size
+screen = turtle.Screen()
+screen.setup(500,500)
+screen.bgcolor("LightSalmon")
+
+#dictionary of colors
+colors = {
+  "purple": "#D9A9EE",
+  "pink": "#F9A6B9",
+  "blue": "#A6C5DC",
+  "yellow": "#FCF879",
+  "green": "#7DFC79",
+  "white": "#FAF3FD"
+}
+
+artist = turtle.Turtle()
+artist.shape("turtle")
+artist.color(colors["white"])
+
+artist.penup()
+artist.goto(0,100)
+
+#(font-family, font-size, font-style)
+style = ("Verdana", 25, "normal")
+artist.write("Happy Thursday!", font = style, align = "center")
+
+artist.goto(0,70)
+
+style2 = ("Verdana", 15, "normal")
+artist.write("Hello to whoever reads this", font = style2, align = "center")
+
+artist.goto(0,50)
+
+artist.write("Thank you for being you <3", font = style2, align = "center")
+
+artist.goto(0,30)
+artist.write(".....", font = style, align = "center")
+
+artist.hideturtle()
